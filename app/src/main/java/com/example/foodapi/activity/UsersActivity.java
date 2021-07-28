15c173c6e882c81c73a26898f98d8f7085525897
Admin <tmstudentler@gmail.com>
2021-07-28 15:31:29 +0300

    step008

--- app/src/main/java/com/example/foodapi/activity/UsersActivity.java
+++ app/src/main/java/com/example/foodapi/activity/UsersActivity.java
@@ -18,6 +18,7 @@ public class UsersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // top menu bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
